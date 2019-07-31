20844f1557cfa67e36716521eb6474d0c72672ff
towerL <513298163@qq.com>
2019-07-31 23:20:39 +0800

    服务器端升级为select模型处理多客户端

--- SocketStepByStep/EasyServer/server.cpp
+++ SocketStepByStep/EasyServer/server.cpp
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <vector>
 
 //三个命令：登入、登出和错误信息
 enum CMD {
@@ -62,6 +63,53 @@ struct LogoutResult: public DataHeader
     int result;
 };
 
+//创建一个动态数组，用来存储socket
+std::vector<int> g_clients;
+
+//处理进程
+int processor(int _cSock) {
+    char szRecv[4096] = {}; //缓冲，用于接收数据
+    //5. 接收客户端的请求数据
+    int nLen = recv(_cSock, szRecv, sizeof(DataHeader), 0);
+    DataHeader* header = (DataHeader*)szRecv;
+    if (nLen <= 0) {
+        printf("nLen <= 0, client has quit!\n");
+        return -1;
+    }
+    //6. 处理请求
+    switch (header->cmd) {
+        case CMD_LOGIN:
+        {
+            recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+            Login* login = (Login*)szRecv;
+            //接收客户端的登陆信息，这里要注意除去headerd部分，因此要加上数据偏移，并且读取范围要减去header的大小
+            printf("receive message: CMD_LOGIN, data length:%d, username:%s, password: %s\n", login->dataLength, login->UserName, login->PassWord); //提示收到命令
+            //假设用户输入正确（这里忽略用户名和密码是否正确的验证过程）
+            LoginResult inret;
+            //7. 向客户端发送数据send
+            send(_cSock, (char*)&inret, sizeof(LoginResult), 0); //向客户端发送登陆结果
+        }
+            break;
+        case CMD_LOGOUT:
+        {
+            recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+            LogOut* logout;
+            printf("receive message: CMD_LOGIN, data length:%d, username:%s\n", logout->dataLength, logout->UserName);
+            LogoutResult outret;
+            //7. 向客户端发送数据send
+            send(_cSock, (char*)&outret, sizeof(LogoutResult), 0);
+        }
+            break;
+        default:
+        {
+            DataHeader header = {0, CMD_ERROR};
+            send(_cSock, (char*)&header, sizeof(DataHeader), 0);
+        }
+            break;
+    }
+    return 0;
+}
+
 int main() {
     //1. 建立一个socket(传入socket族，socket类型, 协议类型)
     int _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -90,62 +138,68 @@ int main() {
         printf("listen success!\n");
     }
     
-    //4. 等待客户端连接accept
-    sockaddr_in clientAddr = {};
-    socklen_t nAddrLen = sizeof(sockaddr_in);
-    int _cSock = accept(_sock, (sockaddr*)&clientAddr, &nAddrLen); //套接字，收到客户端socket地址，返回socket地址的大小
-    if (_cSock == -1) {
-        perror("client socket error!");
-    } else {
-        printf("client socket success! socket = %d, IP = %s \n", _cSock, inet_ntoa(clientAddr.sin_addr)); //打印客户端socket和IP地址
-    }
-    
     //循环接收客户端数据
     while (true) {
-        DataHeader header = {}; //定义消息头结构体
-        //5. 接收客户端的请求数据
-        int nLen = recv(_cSock, (char*)&header, sizeof(header), 0);
-        if (nLen <= 0) {
-            printf("client has quit!\n");
+        //fd_set类型成员包括socket数目和socket数组
+        fd_set fdRead;
+        fd_set fdWrite;
+        fd_set fdExp;
+        
+        //置空
+        FD_ZERO(&fdRead);
+        FD_ZERO(&fdWrite);
+        FD_ZERO(&fdExp);
+        
+        FD_SET(_sock, &fdRead);
+        FD_SET(_sock, &fdWrite);
+        FD_SET(_sock, &fdExp);
+        
+        //循环的加入数据
+        for (int n = (int)g_clients.size() - 1; n >= 0 ; --n) {
+            //将新加入的客户端放到可读集合中用于查询
+            FD_SET(g_clients[n], &fdRead);
+        }
+        
+        //select模型(nfds是指FED_SET集合中所有描述符（socket）的范围，而不是数量，即是所有文件描述符的最大值+1)
+        int ret = select(_sock + 1, &fdRead, &fdWrite, &fdExp, NULL);
+        if (ret < 0) {
+            printf("select error, client has quit!\n");
             break;
         }
         
-        //6. 处理请求
-        switch (header.cmd) {
-            case CMD_LOGIN:
-            {
-                Login login = {};
-                recv(_cSock, (char*)&login + sizeof(DataHeader), sizeof(Login) - sizeof(DataHeader), 0); //接收客户端的登陆信息，这里要注意除去headerd部分，因此要加上数据偏移，并且读取范围要减去header的大小
-                printf("receive message: CMD_LOGIN, data length:%d, username:%s, password: %s\n", login.dataLength, login.UserName, login.PassWord); //提示收到命令
-                //假设用户输入正确（这里忽略用户名和密码是否正确的验证过程）
-                LoginResult inret;
-                //7. 向客户端发送数据send
-                send(_cSock, (char*)&inret, sizeof(LoginResult), 0); //向客户端发送登陆结果
+        if (FD_ISSET(_sock, &fdRead)) {
+            FD_CLR(_sock, &fdRead);
+            //4. 等待客户端连接accept
+            sockaddr_in clientAddr = {};
+            socklen_t nAddrLen = sizeof(sockaddr_in);
+            int _cSock = accept(_sock, (sockaddr*)&clientAddr, &nAddrLen); //套接字，收到客户端socket地址，返回socket地址的大小
+            if (_cSock == -1) {
+                perror("client socket error!\n");
+            } else {
+                g_clients.push_back(_cSock);
+                printf("client socket success! socket = %d, IP = %s \n", _cSock, inet_ntoa(clientAddr.sin_addr)); //打印客户端socket和IP地址
             }
-            break;
-            case CMD_LOGOUT:
-            {
-                LogOut logout = {};
-                recv(_cSock, (char*)&logout + sizeof(DataHeader), sizeof(LogOut) - sizeof(DataHeader), 0);
-                printf("receive message: CMD_LOGIN, data length:%d, username:%s\n", logout.dataLength, logout.UserName);
-                LogoutResult outret;
-                //7. 向客户端发送数据send
-                send(_cSock, (char*)&outret, sizeof(LogoutResult), 0);
+        }
+        //循环调用进程处理函数
+        for (int n = g_clients.size() - 1; n >= 0; --n){
+            if (processor(g_clients[n]) == -1){ //如果客户端请求数据长度<=0则从客户端数组中h删除该socket
+                auto it = find(g_clients.begin(), g_clients.end(), g_clients[n]);
+                if (it != g_clients.end())
+                    g_clients.erase(it);
             }
-            break;
-            default:
-                header.cmd = CMD_ERROR;
-                header.dataLength = 0;
-                send(_cSock, (char*)&header, sizeof(DataHeader), 0);
-            break;
         }
         
+    }
+    
+    //循环关闭数组中的socket
+    for (int n = (int)g_clients.size() - 1; n >= 0 ; --n) {
+        close(g_clients[n]);
     }
     
     //8. 关闭套接字close socket
     close(_sock);
     
-    printf("Server has quit!");
+    printf("Server has quit!\n");
     getchar();
     return 0;
 }
